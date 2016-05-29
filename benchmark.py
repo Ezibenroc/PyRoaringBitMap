edfbb46b98598662a27f8f6c04d3ba6b3da07a0a
Tom Cornebize <tom.cornebize@ensimag.grenoble-inp.fr>
2016-05-29 12:55:27 +0200

    Rewrite benchmark.

--- benchmark.py
+++ benchmark.py
@@ -3,115 +3,175 @@
 import time
 import random
 import numpy
+import abc
 from pyroaring import BitMap
 
-class BenchMark:
+class AbstractBenchMark(metaclass=abc.ABCMeta):
+    max_int = 2**32
     classes = [BitMap, set]
-    nb_repeat = 1
-    size = 10000000
+    nb_repeat = 100
 
-    @staticmethod
-    def measure_constructor(cls, values):
+    def __init__(self, sample_sizes, agregators=[min, max, numpy.mean, numpy.median]):
+        self.sample_sizes = sample_sizes
+        self.agregators = agregators
+
+    def iter_sizes(self):
+        try:
+            for size in self.sample_sizes:
+                assert size > 0 and size < self.max_int
+                yield size
+        except TypeError: # not iterable
+            size = self.sample_sizes
+            assert isinstance(size, int) and size > 0 and size < 2**32
+            yield size
+
+    @abc.abstractmethod
+    def run(self, size):
+        '''
+            Return a list of floats. Result[i] is the run-time of the class self.classes[i].
+        '''
+        pass
+
+    def run_all(self, size):
+        return [self.run(size) for _ in range(self.nb_repeat)]
+
+    def agregate(self, measures, agregate_function):
+        result = []
+        for i in range(len(self.classes)):
+            result.append(agregate_function([m[i] for m in measures]))
+        return result
+
+    def run_and_agregate(self, r_dict=None):
+        r_dict = r_dict or dict()
+        r_func = r_dict[self.__class__.__name__] = dict()
+        for size in self.iter_sizes():
+            r_size = r_func[size] = dict()
+            results = self.run_all(size)
+            for i, cls in enumerate(self.classes):
+                r_class = r_size[cls.__name__] = dict()
+                for agregator in self.agregators:
+                    result = self.agregate(results, agregator)
+                    r_agr = r_class[agregator.__name__] = result[i]
+        return r_dict
+
+    @classmethod
+    def print_results(cls, results):
+        for func, d_func in sorted(results.items()):
+            print('\n# %s' % func)
+            for size, d_size in sorted(d_func.items()):
+                print('\tSize=%d' % size)
+                for cls, d_cls in sorted(d_size.items()):
+                    print('\t\tClass %s' % cls)
+                    for agregator, value in sorted(d_cls.items()):
+                        print('\t\t\t%s: %.4f s.' % (agregator.ljust(8), value))
+
+    @classmethod
+    def get_random_range(cls, size):
+        start = random.randint(0, cls.max_int-size)
+        max_step = (cls.max_int-start)//size
+        if random.random() < 0.5: # compact sample
+            step = random.randint(1, min(max_step, 32))
+        else: # sparse sample
+            step = random.randint(1, max_step)
+        end = start + step*size
+        return range(start, end, step)
+
+
+class AbstractConstructorBenchMark(AbstractBenchMark):
+
+    @abc.abstractmethod
+    def get_values(self, size):
+        pass
+
+    def run_cls(self, cls, values):
         begin = time.time()
         result = cls(values)
         return time.time()-begin
 
-    @staticmethod
-    def measure_constructor_classes(classes, values):
-        measures = []
-        for cls in classes:
-            measures.append(BenchMark.measure_constructor(cls, values))
-        return measures
+    def run(self, size):
+        values = self.get_values(size)
+        assert len(values) == size
+        return [self.run_cls(cls, values) for cls in self.classes]
 
-    @staticmethod
-    def measure_copy_constructor(cls, values):
+class ListConstructor(AbstractConstructorBenchMark):
+
+    def get_values(self, size):
+        if random.random() < 0.5: # compact sample
+            universe = range(min(self.max_int, size*2))
+        else: # sparse sample
+            universe = range(self.max_int)
+        return random.sample(universe, size)
+
+
+class RangeConstructor(AbstractConstructorBenchMark):
+
+    def get_values(self, size):
+        return self.get_random_range(size)
+
+
+class ContinuousRangeConstructor(RangeConstructor):
+
+    def get_values(self, size):
+        return range(size)
+
+class CopyConstructor(AbstractConstructorBenchMark):
+
+    def get_values(self, size):
+        return self.get_random_range(size)
+
+    def run_cls(self, cls, values):
         values = cls(values)
-        return BenchMark.measure_constructor(cls, values)
+        return super().run_cls(cls, values)
 
-    @staticmethod
-    def measure_copy_constructor_classes(classes, values):
-        measures = []
-        for cls in classes:
-            measures.append(BenchMark.measure_copy_constructor(cls, values))
-        return measures
+class AbstractBinaryOpBenchMark(AbstractBenchMark):
 
-    @staticmethod
-    def measure_binary_operation(cls, values1, values2, op):
+    @abc.abstractmethod
+    def do_binary_op(self, values1, values2):
+        pass
+
+    def get_values(self, size):
+        values = self.get_random_range(size)
+        assert len(values) == size
+        return values
+
+    def run_cls(self, cls, values1, values2):
         values1 = cls(values1)
         values2 = cls(values2)
         begin = time.time()
-        result = op(values1, values2)
+        self.do_binary_op(values1, values2)
         return time.time()-begin
 
-    @staticmethod
-    def measure_binary_operation_classes(classes, values1, values2, op):
-        measures = []
-        for cls in classes:
-            measures.append(BenchMark.measure_binary_operation(cls, values1, values2, op))
-        return measures
+    def run(self, size):
+        values1 = self.get_values(size)
+        values2 = self.get_values(size)
+        return [self.run_cls(cls, values1, values2) for cls in self.classes]
 
-    def list_constructor(self):
-        values = random.sample(range(self.size), self.size//2)
-        return self.measure_constructor_classes(self.classes, values)
+class Union(AbstractBinaryOpBenchMark):
 
-    def get_random_range(self):
-        start = random.randint(0, 2**16)
-        end = random.randint(self.size//2, self.size)
-        return range(start, end)
+    def do_binary_op(self, values1, values2):
+        values1 | values2
 
-    def range_constructor(self):
-        values = self.get_random_range()
-        return self.measure_constructor_classes(self.classes, values)
+class Intersection(AbstractBinaryOpBenchMark):
 
-    def copy_constructor(self):
-        values = self.get_random_range()
-        return self.measure_copy_constructor_classes(self.classes, values)
+    def do_binary_op(self, values1, values2):
+        values1 & values2
 
-    def union(self):
-        values1 = self.get_random_range()
-        values2 = self.get_random_range()
-        return self.measure_binary_operation_classes(self.classes, values1, values2, lambda s1, s2: s1|s2)
+class UnionInPlace(AbstractBinaryOpBenchMark):
 
-    def intersection(self):
-        values1 = self.get_random_range()
-        values2 = self.get_random_range()
-        return self.measure_binary_operation_classes(self.classes, values1, values2, lambda s1, s2: s1&s2)
+    def do_binary_op(self, values1, values2):
+        values1 |= values2
 
-    def union_inplace(self):
-        values1 = self.get_random_range()
-        values2 = self.get_random_range()
-        return self.measure_binary_operation_classes(self.classes, values1, values2, lambda s1, s2: s1.__ior__(s2))
+class IntersectionInPlace(AbstractBinaryOpBenchMark):
 
-    def intersection_inplace(self):
-        values1 = self.get_random_range()
-        values2 = self.get_random_range()
-        return self.measure_binary_operation_classes(self.classes, values1, values2, lambda s1, s2: s1.__iand__(s2))
-
-    def measure_time(self, measurer):
-        measures = []
-        for _ in range(self.nb_repeat):
-            measures.append(measurer())
-        result = []
-        for i in range(len(self.classes)):
-            result.append(numpy.mean([m[i] for m in measures]))
-        return result
-
-    def print_result(self, measurer):
-        print(measurer.__name__)
-        result = self.measure_time(measurer)
-        for i, t in enumerate(result):
-            print('\t%s: %.3fs, ratio: %.3f' % (self.classes[i].__name__.ljust(10), t, t/result[0]))
-        print('')
-
-    def print_results(self):
-        self.print_result(self.list_constructor)
-        self.print_result(self.range_constructor)
-        self.print_result(self.copy_constructor)
-        self.print_result(self.union)
-        self.print_result(self.intersection)
-        self.print_result(self.union_inplace)
-        self.print_result(self.intersection_inplace)
+    def do_binary_op(self, values1, values2):
+        values1 &= values2
 
 if __name__ == '__main__':
-    bench = BenchMark()
-    bench.print_results()
+    sample_sizes = [2**n for n in range(10, 17)]
+    classes = [ListConstructor, RangeConstructor, ContinuousRangeConstructor, CopyConstructor,
+        Union, Intersection, UnionInPlace, IntersectionInPlace]
+    result_dict = None
+    for cls in classes:
+        print('Run %s...' % cls.__name__)
+        result_dict = cls(sample_sizes).run_and_agregate(result_dict)
+    AbstractBenchMark.print_results(result_dict)
