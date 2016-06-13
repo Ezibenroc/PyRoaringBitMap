56f54e3b0ab393384962229e2872df66e572b851
Tom Cornebize <tom.cornebize@ensimag.grenoble-inp.fr>
2016-06-13 20:01:50 +0200

    Update benchmark (add ManyUnion, display ratios).

--- benchmark.py
+++ benchmark.py
@@ -9,7 +9,7 @@ from pyroaring import BitMap
 class AbstractBenchMark(metaclass=abc.ABCMeta):
     max_int = 2**32
     classes = [BitMap, set]
-    nb_repeat = 100
+    nb_repeat = 10
 
     def __init__(self, sample_sizes, agregators=[min, max, numpy.mean, numpy.median]):
         self.sample_sizes = sample_sizes
@@ -54,16 +54,21 @@ class AbstractBenchMark(metaclass=abc.ABCMeta):
                     r_agr = r_class[agregator.__name__] = result[i]
         return r_dict
 
-    @classmethod
-    def print_results(cls, results):
-        for func, d_func in sorted(results.items()):
+    @staticmethod
+    def print_results(comparison_class, results, agregators_to_print=None):
+        for func, d_func in sorted(results.items(), key=(lambda t: '#'+t[0] if 'Constructor' in t[0] else t[0])):
             print('\n# %s' % func)
             for size, d_size in sorted(d_func.items()):
                 print('\tSize=%d' % size)
                 for cls, d_cls in sorted(d_size.items()):
                     print('\t\tClass %s' % cls)
-                    for agregator, value in sorted(d_cls.items()):
-                        print('\t\t\t%s: %.4f s.' % (agregator.ljust(8), value))
+                    agregators_to_print = agregators_to_print or d_cls.keys()
+                    for agregator in agregators_to_print:
+                        value = d_cls[agregator]
+                        print('\t\t\t%s: %.4f s.' % (agregator.ljust(6), value))
+                    for agregator in agregators_to_print:
+                        if cls != comparison_class:
+                            print('\t\t\tRatio of %s with %s: %.4f' % (agregator.ljust(6), comparison_class, value/d_size[comparison_class][agregator]))
 
     @classmethod
     def get_random_range(cls, size):
@@ -146,32 +151,62 @@ class AbstractBinaryOpBenchMark(AbstractBenchMark):
         values2 = self.get_values(size)
         return [self.run_cls(cls, values1, values2) for cls in self.classes]
 
-class Union(AbstractBinaryOpBenchMark):
+class BinaryUnion(AbstractBinaryOpBenchMark):
 
     def do_binary_op(self, values1, values2):
         values1 | values2
 
-class Intersection(AbstractBinaryOpBenchMark):
+class BinaryIntersection(AbstractBinaryOpBenchMark):
 
     def do_binary_op(self, values1, values2):
         values1 & values2
 
-class UnionInPlace(AbstractBinaryOpBenchMark):
+class BinaryUnionInPlace(AbstractBinaryOpBenchMark):
 
     def do_binary_op(self, values1, values2):
         values1 |= values2
 
-class IntersectionInPlace(AbstractBinaryOpBenchMark):
+class BinaryIntersectionInPlace(AbstractBinaryOpBenchMark):
 
     def do_binary_op(self, values1, values2):
         values1 &= values2
 
+class AbstractManyOpBenchMark(AbstractBenchMark):
+
+    @abc.abstractmethod
+    def do_many_op(self, values):
+        pass
+
+    def get_values(self, size):
+        values = self.get_random_range(size)
+        assert len(values) == size
+        return values
+
+    def run_cls(self, cls, values_list):
+        values_list = [cls(v) for v in values_list]
+        begin = time.time()
+        self.do_many_op(cls, *values_list)
+        return time.time()-begin
+
+    def run(self, size):
+        nb = random.randint(2, 100)
+        values_list = [self.get_values(size) for _ in range(nb)]
+        return [self.run_cls(cls, values_list) for cls in self.classes]
+
+class ManyUnion(AbstractManyOpBenchMark):
+
+    def do_many_op(self, cls, *values):
+        cls.union(*values)
+
 if __name__ == '__main__':
-    sample_sizes = [2**n for n in range(10, 17)]
+    total_time = time.time()
+    sample_sizes = [2**n for n in range(15, 20)]
     classes = [ListConstructor, RangeConstructor, ContinuousRangeConstructor, CopyConstructor,
-        Union, Intersection, UnionInPlace, IntersectionInPlace]
+        BinaryUnion, BinaryIntersection, BinaryUnionInPlace, BinaryIntersectionInPlace, ManyUnion]
     result_dict = None
     for cls in classes:
         print('Run %s...' % cls.__name__)
         result_dict = cls(sample_sizes).run_and_agregate(result_dict)
-    AbstractBenchMark.print_results(result_dict)
+    total_time = time.time()-total_time
+    AbstractBenchMark.print_results('BitMap', result_dict, agregators_to_print=['median', 'mean'])
+    print('\nTotal time: %.2f s.' % total_time)
