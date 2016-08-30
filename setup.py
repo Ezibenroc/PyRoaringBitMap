453e295f91d82a4c511da8fbf8077d4826e69321
Tom Cornebize <tom.cornebize@ensimag.grenoble-inp.fr>
2016-08-30 19:48:55 +0200

    Increase version number.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pyroaring',
-    version='0.0.1',
+    version='0.0.2',
     description='Fast and lightweight set for unsigned 32 bits integers.',
     url='https://github.com/Ezibenroc/PyRoaringBitMap',
     author='Tom Cornebize',
