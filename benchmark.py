02f7fb35bed686397c9462bbe65b8487a05e60dc
Tom Cornebize <tom.cornebize@ensimag.grenoble-inp.fr>
2016-08-31 17:25:25 +0200

    Rewrite the benchmark.
    * Now, that we have an efficient list constructor, we can run tests with better samples (i.e. random lists instead of random ranges).
    * We plot the ratios rather than the run times.
    * The benchmark can now be properly stopped at any time.

--- benchmark.py
+++ benchmark.py
@@ -6,142 +6,119 @@ import numpy
 import abc
 import pickle
 import argparse
+import sys
 from pyroaring import BitMap
 
 class AbstractBenchMark(metaclass=abc.ABCMeta):
     max_int = 2**32
+    sizes = [2**n for n in range(0, 22)]
     classes = [BitMap, set]
-    nb_repeat = 20
     agregators=[min, max, numpy.mean, numpy.median]
 
-    def __init__(self, sample_sizes):
-        self.sample_sizes = sample_sizes
-
-    def iter_sizes(self):
-        try:
-            for size in self.sample_sizes:
-                assert isinstance(size, int) and size > 0 and size < self.max_int
-                yield size
-        except TypeError: # not iterable
-            size = self.sample_sizes
-            assert isinstance(size, int) and size > 0 and size < self.max_int
-            yield size
-
-    @abc.abstractmethod
-    def run(self, size):
-        '''
-            Return a list of floats. Result[i] is the run-time of the class self.classes[i].
-        '''
-        pass
-
-    def run_all(self, size):
-        return [self.run(size) for _ in range(self.nb_repeat)]
-
-    def run_and_agregate(self, r_dict=None):
-        r_dict = r_dict or dict()
-        r_func = r_dict[self.__class__.__name__] = dict()
-        for size in self.iter_sizes():
-            r_size = r_func[size] = dict()
-            results = self.run_all(size)
-            for i, cls in enumerate(self.classes):
-                r_class = r_size[cls.__name__] = [r[i] for r in results]
-        return r_dict
+    @classmethod
+    def get_data(cls, size, bound):
+        assert size < cls.max_int
+        assert bound < cls.max_int
+        assert size < bound
+        return random.sample(range(0, bound), size)
 
     @classmethod
-    def print_results(cls, comparison_class, results, agregators_to_print=None):
-        if agregators_to_print is None:
-            agregators_to_print = cls.agregators
-        for func, d_func in sorted(results.items(), key=(lambda t: '#'+t[0] if 'Constructor' in t[0] else t[0])):
-            print('\n# %s' % func)
-            for size, d_size in sorted(d_func.items()):
-                print('\tSize=%d' % size)
-                for test_class, values in sorted(d_size.items()):
-                    print('\t\tClass %s' % test_class)
-                    for agregator in agregators_to_print:
-                        value = agregator(values)
-                        print('\t\t\t%s: %.4f s.' % (agregator.__name__.ljust(6), value))
-                    for agregator in agregators_to_print:
-                        if test_class != comparison_class:
-                            print('\t\t\tRatio of %s with %s: %.4f' % (agregator.__name__.ljust(6), comparison_class, agregator(values)/agregator(d_size[comparison_class])))
+    def sparse_data(cls, size):
+        bound = size*16
+        return cls.get_data(size, bound)
+
+    @classmethod
+    def dense_data(cls, size):
+        bound = size*2
+        return cls.get_data(size, bound)
+
+    def __init__(self):
+        self.results = {data_func : dict() for data_func in [self.sparse_data, self.dense_data]}
+        for data_func, d in self.results.items():
+            for size in self.sizes:
+                d[size] = {cls:list() for cls in self.classes}
+
+    def compute_ratios(self):
+        self.ratios = {data_func : dict() for data_func in self.results.keys()}
+        for data_func, d in self.ratios.items():
+            for size in self.sizes:
+                d[size] = {cls:list() for cls in self.classes[1:]}
+                for cls in self.classes[1:]:
+                    r = list()
+                    result = self.results[data_func][size]
+                    for i in range(len(result[cls])):
+                        r.append(result[cls][i]/result[self.classes[0]][i])
+                    d[size][cls] = numpy.mean(r)
+
+    @abc.abstractmethod
+    def run(self, data_func, size):
+        pass
+
+    def run_all(self):
+        for data_func in self.results.keys():
+            for size in self.sizes:
+                times = self.run(data_func, size)
+                for i, cls in enumerate(self.classes):
+                    self.results[data_func][size][cls].append(times[i])
+
+    def print_results(self):
+        print('\n# %s' % self.__class__.__name__)
+        for data_func, d in sorted(self.ratios.items(), key = lambda x: x[0].__name__):
+            print('\t%s' % data_func.__name__)
+            for size, d_size in sorted(d.items()):
+                print('\t\tSize=%d' % size)
+                for test_class, ratio in sorted(d_size.items(), key = lambda x: x[0].__name__):
+                    print('\t\t\tRatio %s/%s: %f' % (test_class.__name__, self.classes[0].__name__, ratio))
 
     @staticmethod
-    def _results_to_tex_noagreg(f, results): # results has the form { sizeA : [vA1, vA2, ..., vAn], sizeB : [vB1, vB2, ..., vBm], ...}
-        f.write('\t\t\t\\addplot [only marks] table {\n')
-        for size, values in sorted(results.items()):
-            for value in values:
-                f.write('\t\t\t\t%d %f\n' % (size, value))
-        f.write('\t\t\t};\n')
-
-    @staticmethod
-    def _results_to_tex_agreg(f, results, agregator): # results has the form { sizeA : [vA1, vA2, ..., vAn], sizeB : [vB1, vB2, ..., vBm], ...}
+    def _results_to_tex(f, results): # results has the form { size1 : result1, size2 : result2, ...}
         f.write('\t\t\t\\addplot table {\n')
-        for size, values in sorted(results.items()):
-            value = agregator(values)
+        for size, value in sorted(results.items()):
             f.write('\t\t\t\t%d %f\n' % (size, value))
         f.write('\t\t\t};\n')
 
     @staticmethod
     def add_legend(f, legend):
-        f.write('\t\t\t\\addlegendentry{%s}\n' % legend)
+        f.write('\t\t\t\\addlegendentry{%s}\n' % legend.replace('_', '\\_'))
 
-    @classmethod
-    def _plot_func(cls, f, d_func, print_all_points, agregators_to_print):
+    def _plot_func(self, f):
         f.write('\t\\begin{tikzpicture}\n')
-        f.write('\t\t\\begin{loglogaxis}[\n')
+        f.write('\t\t\\begin{axis}[\n')
         f.write('\t\t\txlabel=size,\n')
-        f.write('\t\t\tylabel=run time(s),\n')
+        f.write('\t\t\txmode=log,\n')
+        f.write('\t\t\tymode=log,\n')
+        f.write('\t\t\tylabel=speedup,\n')
         f.write('\t\t\tlegend style={at={(0, 1)},anchor=north west,fill=none}\n')
         f.write('\t\t\t]\n')
-        results = dict()
-        # First, we "inverse" the dict...
-        for size, d_size in d_func.items():
-            for test_class, values in d_size.items():
-                try:
-                    results[test_class][size] = values
-                except KeyError:
-                    results[test_class] = {size : values}
-        # Then, we print the points
-        for test_class, d_size in sorted(results.items()):
-            if print_all_points:
-                cls._results_to_tex_noagreg(f, d_size)
-            for agregator in agregators_to_print:
-                cls._results_to_tex_agreg(f, d_size, agregator)
+        for data_func, d_func in sorted(self.ratios.items(), key=lambda x: x[0].__name__):
+            results = dict()
+            # First, we "inverse" the dict...
+            for size, d_size in d_func.items():
+                for test_class, value in d_size.items():
+                    try:
+                        results[test_class][size] = value
+                    except KeyError:
+                        results[test_class] = {size : value}
+            for test_class in self.classes[1:]:
+                self._results_to_tex(f, results[test_class])
         # Finally, the legend
-        for test_class, d_size in sorted(results.items()):
-            if print_all_points:
-                cls.add_legend(f, '%s: all points' % test_class)
-            for agregator in agregators_to_print:
-                cls.add_legend(f, '%s: %s' % (test_class, agregator.__name__))
-        f.write('\t\t\\end{loglogaxis}\n')
+        for data_func in sorted(self.ratios, key=lambda x: x.__name__):
+            for test_class in self.classes[1:]:
+                self.add_legend(f, 'ratio %s/%s: %s' % (test_class.__name__, self.classes[0].__name__, data_func.__name__))
+        f.write('\t\t\\end{axis}\n')
         f.write('\t\\end{tikzpicture}\n')
 
-    @classmethod
-    def plot_results(cls, f, results, print_all_points=True, agregators_to_print=None):
-        if agregators_to_print is None:
-            agregators_to_print = cls.agregators
+    def plot_results(self, f):
         f.write('% Do not forget \\usepackage{pgfplots}\n')
-        for func, d_func in sorted(results.items(), key=(lambda t: '#'+t[0] if 'Constructor' in t[0] else t[0])):
-            f.write('\\begin{figure}\n')
-            cls._plot_func(f, d_func, print_all_points, agregators_to_print)
-            f.write('\t\\caption{%s}\n' % func)
-            f.write('\\end{figure}\n')
-
-    @classmethod
-    def get_random_range(cls, size):
-        start = random.randint(0, cls.max_int-size)
-        max_step = (cls.max_int-start)//size
-        if random.random() < 0.5: # compact sample
-            step = random.randint(1, min(max_step, 32))
-        else: # sparse sample
-            step = random.randint(1, max_step)
-        end = start + step*size
-        return range(start, end, step)
-
+        f.write('\\begin{figure}\n')
+        self._plot_func(f)
+        f.write('\t\\caption{%s}\n' % self.__class__.__name__)
+        f.write('\\end{figure}\n')
 
 class AbstractConstructorBenchMark(AbstractBenchMark):
 
     @abc.abstractmethod
-    def get_values(self, size):
+    def get_values(self, data_func, size):
         pass
 
     def run_cls(self, cls, values):
@@ -149,36 +126,34 @@ class AbstractConstructorBenchMark(AbstractBenchMark):
         result = cls(values)
         return time.time()-begin
 
-    def run(self, size):
-        values = self.get_values(size)
+    def run(self, data_func, size):
+        values = self.get_values(data_func, size)
         assert len(values) == size
         return [self.run_cls(cls, values) for cls in self.classes]
 
 class ListConstructor(AbstractConstructorBenchMark):
 
-    def get_values(self, size):
-        if random.random() < 0.5: # compact sample
-            universe = range(min(self.max_int, size*2))
-        else: # sparse sample
-            universe = range(self.max_int)
-        return random.sample(universe, size)
-
+    def get_values(self, data_func, size):
+        return data_func(size)
 
 class RangeConstructor(AbstractConstructorBenchMark):
 
-    def get_values(self, size):
-        return self.get_random_range(size)
-
-
-class ContinuousRangeConstructor(RangeConstructor):
-
-    def get_values(self, size):
-        return range(size)
+    def get_values(self, data_func, size):
+        start = random.randint(0, self.max_int-size)
+        max_step = (self.max_int-start)//size
+        if data_func == self.sparse_data:
+            step = random.randint(max_step//2, max_step)
+        elif data_func == self.dense_data:
+            step = random.randint(1, min(max_step, 4))
+        else:
+            assert False
+        end = start + step*size
+        return range(start, end, step)
 
 class CopyConstructor(AbstractConstructorBenchMark):
 
-    def get_values(self, size):
-        return self.get_random_range(size)
+    def get_values(self, data_func, size):
+        return data_func(size)
 
     def run_cls(self, cls, values):
         values = cls(values)
@@ -190,11 +165,6 @@ class AbstractBinaryOpBenchMark(AbstractBenchMark):
     def do_binary_op(self, values1, values2):
         pass
 
-    def get_values(self, size):
-        values = self.get_random_range(size)
-        assert len(values) == size
-        return values
-
     def run_cls(self, cls, values1, values2):
         values1 = cls(values1)
         values2 = cls(values2)
@@ -202,9 +172,9 @@ class AbstractBinaryOpBenchMark(AbstractBenchMark):
         self.do_binary_op(values1, values2)
         return time.time()-begin
 
-    def run(self, size):
-        values1 = self.get_values(size)
-        values2 = self.get_values(size)
+    def run(self, data_func, size):
+        values1 = data_func(size)
+        values2 = data_func(size)
         return [self.run_cls(cls, values1, values2) for cls in self.classes]
 
 class BinaryUnion(AbstractBinaryOpBenchMark):
@@ -243,20 +213,15 @@ class AbstractManyOpBenchMark(AbstractBenchMark):
     def do_many_op(self, values):
         pass
 
-    def get_values(self, size):
-        values = self.get_random_range(size)
-        assert len(values) == size
-        return values
-
     def run_cls(self, cls, values_list):
         values_list = [cls(v) for v in values_list]
         begin = time.time()
         self.do_many_op(cls, *values_list)
         return time.time()-begin
 
-    def run(self, size):
-        nb = random.randint(2, 100)
-        values_list = [self.get_values(size) for _ in range(nb)]
+    def run(self, data_func, size):
+        nb = 20#random.randint(2, 100)
+        values_list = [data_func(size) for _ in range(nb)]
         return [self.run_cls(cls, values_list) for cls in self.classes]
 
 class ManyUnion(AbstractManyOpBenchMark):
@@ -264,8 +229,50 @@ class ManyUnion(AbstractManyOpBenchMark):
     def do_many_op(self, cls, *values):
         cls.union(*values)
 
-if __name__ == '__main__':
+classes = [ListConstructor, RangeConstructor, CopyConstructor,
+    BinaryUnion, BinaryIntersection, BinarySymmetricDifference,
+    BinaryUnionInPlace, BinaryIntersectionInPlace, BinarySymmetricDifferenceInPlace,
+    ManyUnion]
 
+class Runer:
+    def __init__(self):
+        self.benchmarks = [cls() for cls in classes]
+        self.nb_measures = 0
+
+    def run(self):
+        begin = time.time()
+        while True:
+            print('Number of measures: %d' % self.nb_measures)
+            try:
+                for bench in self.benchmarks:
+                    print('\t%s' % bench.__class__.__name__)
+                    bench.run_all()
+            except KeyboardInterrupt:
+                end = time.time()
+                break
+            self.nb_measures += 1
+        for bench in self.benchmarks:
+            bench.compute_ratios()
+        print('\nFinished after a time of %fs.' % (end-begin))
+
+    def print_results(self):
+        for bench in self.benchmarks:
+            bench.print_results()
+
+    def plot_results(self, file_name):
+        with open(file_name, 'w') as f:
+            f.write('%% Number of measures: %d\n' % self.nb_measures)
+            f.write('\\documentclass{article}\n')
+            f.write('\\usepackage{tikz, pgfplots}\n')
+            f.write('\\begin{document}\n')
+            f.write('\n')
+            for bench in self.benchmarks:
+                bench.plot_results(f)
+                f.write('\n')
+            f.write('\\end{document}')
+
+
+if __name__ == '__main__':
     # Parse arguments
     parser = argparse.ArgumentParser(
             description='Benchmark for pyroaring')
@@ -278,30 +285,19 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.load is None:
-        total_time = time.time()
-        sample_sizes = [2**n for n in range(5, 20)]
-        classes = [ListConstructor, RangeConstructor, ContinuousRangeConstructor, CopyConstructor,
-            BinaryUnion, BinaryIntersection, BinarySymmetricDifference,
-            BinaryUnionInPlace, BinaryIntersectionInPlace, BinarySymmetricDifferenceInPlace,
-            ManyUnion]
-        result_dict = None
-        for cls in classes:
-            print('Run %s...' % cls.__name__)
-            result_dict = cls(sample_sizes).run_and_agregate(result_dict)
-        total_time = time.time()-total_time
+        runer = Runer()
     else:
         with open(args.load, 'rb') as f:
-            result_dict = pickle.load(f)
-        print('Loaded results from file %s' % args.load)
-    AbstractBenchMark.print_results('BitMap', result_dict, agregators_to_print=[numpy.mean, numpy.median])
+            runer = pickle.load(f)
+        print('Loaded results from file %s\n' % args.load)
+    print('Now launching the benchmark. You can stop it properly at any time by pressing Ctrl-C (results will then be written and plotted).\n')
+    runer.run()
+    runer.print_results()
     print('')
     if args.plot is not None:
-        with open(args.plot, 'w') as f:
-            AbstractBenchMark.plot_results(f, result_dict, print_all_points=False, agregators_to_print=[numpy.mean])
+        runer.plot_results(args.plot)
         print('\nPlots written in file %s' % args.plot)
     if args.dump is not None:
         with open(args.dump, 'wb') as f:
-            pickle.dump(result_dict, f)
+            pickle.dump(runer, f)
         print('Results dumped in file %s' % args.dump)
-    if args.load is None:
-        print('Total time: %.2f s.' % total_time)
