c9e4c7335e2753ec58713f1158fdb9e8ce0d3c06
Tom Cornebize <tom.cornebize@ensimag.grenoble-inp.fr>
2016-09-12 13:25:18 +0200

    Add classifiers for pypi, update version number.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pyroaring',
-    version='0.0.2',
+    version='0.0.3',
     description='Fast and lightweight set for unsigned 32 bits integers.',
     url='https://github.com/Ezibenroc/PyRoaringBitMap',
     author='Tom Cornebize',
@@ -12,5 +12,15 @@ setup(
     license='MIT',
     packages=[
         'pyroaring',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 )
