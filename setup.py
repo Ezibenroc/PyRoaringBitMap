61dfd6a18b19573b0414d99a75e97f56c4e65edb
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2021-07-06 19:10:54 +0200

    Drop the support for Python 3.5

--- setup.py
+++ setup.py
@@ -126,7 +126,6 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
