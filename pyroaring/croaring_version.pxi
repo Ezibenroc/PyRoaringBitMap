5c09d425e220b9306700c054b8da80a20ec4ed8e
Daniel Lemire <lemire@gmail.com>
2023-02-28 19:09:15 -0500

    Updating to v0.9.9

--- pyroaring/croaring_version.pxi
+++ pyroaring/croaring_version.pxi
@@ -1,1 +1,1 @@
-__croaring_version__ = "v0.9.8"
+__croaring_version__ = "v0.9.9"
--- pyroaring/roaring.c
+++ pyroaring/roaring.c
@@ -1,5 +1,5 @@
 // !!! DO NOT EDIT - THIS IS AN AUTO-GENERATED FILE !!!
-// Created by amalgamation.sh on 2023-02-16T17:16:21Z
+// Created by amalgamation.sh on 2023-02-28T23:30:28Z
 
 /*
  * The CRoaring project is under a dual license (Apache/MIT).
