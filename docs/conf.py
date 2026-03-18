c740e99b4a3be376239192361318e3a24e8d8c85
Tom Cornebize <tom.cornebize@gmail.com>
2026-03-18 22:13:50 +0100

    Version 1.0.4

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ author = 'Tom Cornebize'
 # built documents.
 #
 # The short X.Y version.
-version = '1.0.3'
+version = '1.0.4'
 # The full version, including alpha/beta/rc tags.
 release = version
 
